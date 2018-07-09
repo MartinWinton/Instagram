20128647959d68b93ae70be4ef17c4d6294a98c4
Martin Winton <mwinton@fb.com>
2018-07-09 13:00:56 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Martin Winton on 7/9/18.
+//  Copyright © 2018 Martin Winton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Martin Winton on 7/9/18.
+//  Copyright © 2018 Martin Winton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
